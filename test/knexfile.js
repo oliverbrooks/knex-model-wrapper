def1da0b088e67c99313d4e406285882bda2ddaf
Oliver Brooks <oli@pearlshare.com>
2015-10-19 22:59:10 +0100

    add circleci configuration

--- test/knexfile.js
+++ test/knexfile.js
@@ -3,6 +3,6 @@ module.exports = {
   test: {
     client: "pg",
     dialect: "postgres",
-    connection: process.env.PG_CONNECTION || "postgres://postgres:@127.0.0.1:5432/knex_model_wrapper_test"
+    connection: process.env.DATABASE_URL || "postgres://postgres:@127.0.0.1:5432/knex_model_wrapper_test"
   }
 };
